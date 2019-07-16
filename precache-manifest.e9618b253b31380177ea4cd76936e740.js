7be779bd988f530d3ce6405e111086c0f5431e54
Abhiraj Pundir <pundirabhiraj.com>
2019-07-16 23:58:38 +0530

    Updates

---
+++ precache-manifest.e9618b253b31380177ea4cd76936e740.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac45c4529c38e7e1c000f73bb84dc9f",
+    "url": "/react-dj/index.html"
+  },
+  {
+    "revision": "838deba85d07417435ef",
+    "url": "/react-dj/static/js/2.30771d76.chunk.js"
+  },
+  {
+    "revision": "c58fd1b395dc577d9cf8",
+    "url": "/react-dj/static/js/main.6c95729d.chunk.js"
+  },
+  {
+    "revision": "41e2ad4f7ec89a00a14b",
+    "url": "/react-dj/static/js/runtime~main.60b6c520.js"
+  }
+]);
